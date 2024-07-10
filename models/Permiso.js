d20b491c936db32f20ae30802df82c6f5c459612
Ismael Mamaní <ismamamani22@gmail.com>
2024-07-10 18:22:43 -0300

    Eliminando todo

--- models/Permiso.js
+++
@@ -1,7 +0,0 @@
-export default class Permiso {
-
-    constructor(id, name) {
-        this.id = id
-        this.name = name
-    }
-}
--- models/UsuarioPermiso.js
+++
@@ -1,7 +0,0 @@
-export default class UsuarioPermiso {
-
-    constructor(usuario_id, permiso_id) {
-        this.usuario_id = usuario_id
-        this.permiso_id = permiso_id
-    }
-}
